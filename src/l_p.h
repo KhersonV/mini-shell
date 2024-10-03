68ea6a9e2e886ce01aa0e850e62da3c514a10cb8
Serhii Nazarov <mypmypmew.dev@gmail.com>
2024-10-03 17:52:01 +0200

    linked parser and lexer, added adjusting functino

---
+++ src/l_p.h
@@ -0,0 +1,38 @@
+#ifndef L_P_H
+#define L_P_H
+
+typedef struct s_tree
+{
+	char 			*type;
+	char			*name;
+	int				precedence;
+	struct s_tree	*next;
+	struct s_tree	*prev;
+}					t_tree;
+
+typedef struct s_token
+{
+	char	*str;
+	int		type;
+	int		precedence;
+	struct s_token *next;
+	struct s_token *prev;
+}	t_token;
+
+enum token_types {
+	SPACE = 1,
+	WORD,
+	FIELD,
+	EXP_FIELD,
+	REDIR_OUT,
+	REDIR_IN,
+	REDIR_APPEND,
+	REDIR_INSOURCE,
+	PIPE,
+	END
+};
+
+void print_tokens(t_tree *node);
+t_tree *tokenize(char *s);
+
+#endif
--- src/parser.c
+++ src/parser.c
@@ -6,7 +6,7 @@
 /*   By: lynchsama <lynchsama@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/07 12:53:34 by vmamoten          #+#    #+#             */
-/*   Updated: 2024/10/03 17:08:50 by lynchsama        ###   ########.fr       */
+/*   Updated: 2024/10/03 17:50:51 by lynchsama        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "l_p.h"
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	while (*s1 && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+	return ((unsigned char)*s1 - (unsigned char)*s2);
+}
+
 
 typedef struct Node
 {
@@ -56,7 +68,7 @@ void *pop(Stack *stack)
 	if(stack->top == NULL)
 	{
 		printf("Stack is empty");
-		return ;
+		return NULL;
 	}
 
 	Node *temp = stack->top;
@@ -71,7 +83,7 @@ void *peek(Stack *stack)
 	if(stack->top == NULL)
 	{
 		printf("stack is empty");
-		return ;
+		return NULL;
 	}
 	return stack->top->data;
 }
@@ -81,8 +93,47 @@ int isEmpty(Stack *stack)
 	return stack->top == NULL;
 }
 
+void adjusting_token_tree(t_tree **tree)
+{
+	t_tree *curr = *tree;
+
+	while(curr != NULL)
+	{
+		if(!(ft_strcmp(curr->type, "REDIR_IN"))
+		|| !(ft_strcmp(curr->type, "REDIR_OUT"))
+		|| !(ft_strcmp(curr->type, "REDIR_APPEND")))
+		{
+			if(!(ft_strcmp(curr->next->type, "SPACE")))
+				curr = curr->next;
+			if(curr->next != NULL )
+			{
+				curr->next->type = "FILE";
+			}
+		} else if(!(ft_strcmp(curr->type, "REDIR_INSOURCE")))
+		{
+			if(!(ft_strcmp(curr->next->type, "SPACE")))
+				curr = curr->next;
+			if(curr->next != NULL)
+			{
+				curr->next->type = "HEREDOC_MARKER";
+			}
+		}
+
+		curr = curr->next;
+	}
+}
+
 int main()
 {
+	char input[] = "echo \'hello\'	>> file.txt | cat << input.txt";
+	//char input[] = "echo hello >> \"file.txt\" | cat << input.txt";
 
+	t_tree *root;
+	root = tokenize(input);
+	printf("before");
+	print_tokens(root);
+	adjusting_token_tree(&root);
+	printf("after");
+	print_tokens(root);
 	return 0;
 }
