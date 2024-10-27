30999bea0018aa5f173160164b14c3ba7acd3312
Serhii Nazarov <mypmypmew.dev@gmail.com>
2024-10-27 19:15:58 +0100

    basic init

--- src/init.c
+++ src/init.c
@@ -1,9 +1,29 @@
 
 #include "../minishell.h"
 
-int launch_setup()
+int launch_setup(int argc, t_info *info)
 {
+	if(argc != 1 && argc != 3)
+		return -1;
+	if(argc == 3)
+	{
+		info->interactive = 0;
 
+	}
+	else
+		info->interactive = 1;
+	return 1;
+}
 
-	return TRUE;
+int main(int argc, char **argv, char **envp)
+{
+	t_info *info;
+
+	info = malloc(sizeof(t_info));
+	info->envp = copy_envp(envp);
+	info->old_dir = get_env_value(envp, "OLDPWD");
+	printf("%s", info->old_dir);
+	info->exit_status = 0;
+
+	return 0;
 }
