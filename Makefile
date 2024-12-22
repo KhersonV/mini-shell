1bf2b7bcb33b68773b05f92101726a9219fddb0f
Vladyslav Mamotenko <vmamoten@1-B-26.42heilbronn.de>
2024-12-22 15:52:57 +0100

    working with pipes

--- Makefile
+++ Makefile
@@ -27,7 +27,8 @@ SRC = $(SRC_DIR)/main.c \
       $(SRC_DIR)/executor/herodoc.c $(SRC_DIR)/executor/pipes.c $(SRC_DIR)/executor/redirection.c \
       $(SRC_DIR)/lexer/lexer.c $(SRC_DIR)/lexer/lexer_utils.c $(SRC_DIR)/lexer/tokenizer.c \
       $(SRC_DIR)/parser/ast.c $(SRC_DIR)/parser/parser2.c $(SRC_DIR)/parser/parser_utils.c \
-      $(SRC_DIR)/signals/signals.c $(SRC_DIR)/utils/errors.c $(SRC_DIR)/utils/free_utils.c 
+      $(SRC_DIR)/signals/signals.c $(SRC_DIR)/utils/errors.c $(SRC_DIR)/utils/free_utils.c \
+      
 
 # Преобразование исходников в объектные файлы с использованием patsubst
 OBJ = $(patsubst $(SRC_DIR)/%.c, $(OBJ_DIR)/%.o, $(SRC))
--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,44 +6,59 @@
 /*   By: vmamoten <vmamoten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 14:25:20 by vmamoten          #+#    #+#             */
-/*   Updated: 2024/12/22 14:50:19 by vmamoten         ###   ########.fr       */
+/*   Updated: 2024/12/22 15:36:17 by vmamoten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-void	ft_echo(char **args, t_info *info)
+static void print_with_escape(const char *str)
 {
-	int i = 1;       // Начинаем с первого аргумента
-	int newline = 1; // По умолчанию добавляем новую строку
-	// Проверяем, является ли первый аргумент флагом -n
-	while (args[i] && ft_strcmp(args[i], "-n") == 0)
-	{
-		newline = 0; // Если флаг -n найден, не добавляем новую строку
-		i++;
-	}
-	// Вывод остальных аргументов
-	while (args[i])
-	{
-		if (args[i][0] == '$' && ft_strlen(args[i]) > 1)
-			// Если аргумент начинается с $
-		{
-			char *value = get_env_value(info, args[i] + 1);
-				// Ищем переменную окружения
-			if (value)
-				printf("%s", value);
-			else
-				printf(""); // Если переменная не найдена, ничего не выводим
-		}
-		else
-		{
-			printf("%s", args[i]);
-		}
-		if (args[i + 1]) // Добавляем пробел между аргументами
-			printf(" ");
-		i++;
-	}
-	if (newline) // Добавляем новую строку, если флаг -n не был установлен
-		printf("\n");
-	info->exit_status = 0; // Устанавливаем статус выполнения
+    while (*str)
+    {
+        if (*str == '\\' && *(str + 1) != '\0') // Если найден символ '\\'
+        {
+            str++;
+            if (*str == 'n')
+                putchar('\n');
+            else if (*str == 't')
+                putchar('\t');
+            else if (*str == 'r')
+                putchar('\r');
+            else
+                putchar(*str); // Если это не известная последовательность
+        }
+        else
+        {
+            putchar(*str);
+        }
+        str++;
+    }
+}
+
+void ft_echo(char **args, t_info *info)
+{
+    int i = 1; // Начинаем с первого аргумента
+    int newline = 1; // По умолчанию добавляем новую строку
+
+    // Проверяем флаг -n
+    while (args[i] && strcmp(args[i], "-n") == 0)
+    {
+        newline = 0;
+        i++;
+    }
+
+    // Вывод аргументов с обработкой escape-последовательностей
+    while (args[i])
+    {
+        print_with_escape(args[i]); // Обработка строки
+        if (args[i + 1]) // Добавляем пробел между аргументами
+            putchar(' ');
+        i++;
+    }
+
+    if (newline) // Добавляем новую строку, если флаг -n не установлен
+        putchar('\n');
+
+    info->exit_status = 0; // Устанавливаем статус выполнения
 }
