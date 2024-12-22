73924bba6d6ecb837edf45708505a61045a2380b
Serhii Nazarov <mypmypmew.dev@gmail.com>
2024-12-22 09:33:55 +0100

    old tokenizer

--- src/lexer/lexer_utils.c
+++ src/lexer/lexer_utils.c
@@ -61,3 +61,79 @@ void	ft_putchar_fd(char c, int fd)
 {
 	write(fd, &c, 1);
 }
+
+// t_token *tokenize(char *s)
+// {
+// 	t_token *curr = NULL;
+// 	int i = 0;
+// 	char buf[256];
+// 	char variable_buffer[256];
+// 	int buf_index = 0;
+// 	int var_index = 0;
+
+// 	while (s[i] != '\0')
+// 	{
+// 		if (s[i] == '|' || s[i] == '<' || s[i] == '>' || s[i] == ' ' || (s[i] >= 9 && s[i] <= 13))
+// 		{
+// 			if (buf_index > 0)
+// 			{
+// 				buf[buf_index] = '\0';
+// 				curr = add_token(curr, buf, "WORD");
+// 				buf_index = 0;
+// 			}
+// 			curr = add_operator_token(curr, s[i], s[i + 1], &i);
+// 			i++;
+// 		}
+// 		else if (s[i] == '\'' || s[i] == '"')
+// 		{
+// 			if (buf_index > 0)
+// 			{
+// 				buf[buf_index] = '\0';
+// 				curr = add_token(curr, buf, "WORD");
+// 				buf_index = 0;
+// 			}
+// 			if (is_quotes_closed(&s[i]))
+// 			{
+// 				int len = extract_field(&s[i], curr);
+// 				i += (len);
+// 			}
+// 			else
+// 			{
+// 				printf("quotes are not closed, syntax error\n");
+// 				exit(1);
+// 			}
+// 		}
+// 		else if(s[i] == '$')
+// 		{
+// 			if (buf_index > 0)
+// 			{
+// 				buf[buf_index] = '\0';
+// 				curr = add_token(curr, buf, "WORD");
+// 				buf_index = 0;
+// 			}
+// 			var_index = 0;
+// 			i++;
+// 			if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z') || s[i] == '_')
+// 			{
+// 				while ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z') ||
+// 					   s[i] == '_' || (s[i] >= '0' && s[i] <= '9'))
+// 				{
+// 					variable_buffer[var_index++] = s[i++];
+// 				}
+// 				variable_buffer[var_index] = '\0';
+// 				curr = add_token(curr, variable_buffer, "VAR");
+// 			}
+// 		}
+// 		else
+// 		{
+// 			buf[buf_index++] = s[i++];
+// 		}
+// 	}
+// 	if (buf_index > 0)
+// 	{
+// 		buf[buf_index] = '\0';
+// 		curr = add_token(curr, buf, "WORD");
+// 		buf_index = 0;
+// 	}
+// 	return curr;
+// }
