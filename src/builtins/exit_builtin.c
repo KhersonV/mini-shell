90faf456b1b2087fafd5a6b7a18a82d6ccf3f5d4
Vladyslav Mamotenko <vmamoten@1-B-28.42heilbronn.de>
2024-12-31 15:43:22 +0100

    fixed some exit code and some cases in exit builtin

--- src/builtins/exit_builtin.c
+++ src/builtins/exit_builtin.c
@@ -6,24 +6,51 @@
 /*   By: vmamoten <vmamoten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 14:25:26 by vmamoten          #+#    #+#             */
-/*   Updated: 2024/12/16 14:19:52 by vmamoten         ###   ########.fr       */
+/*   Updated: 2024/12/31 15:42:16 by vmamoten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
 #include "../../include/minishell.h"
 
-void ft_exit(char **args, t_info *info)
+int	is_numeric(const char *str)
 {
-    int exit_code = 0;
+    if (*str == '-' || *str == '+')
+        str++;
+    if (!*str)
+        return (0);
+    while (*str)
+    {
+        if (!ft_isdigit(*str))
+            return (0);
+        str++;
+    }
+    return (1);
+}
+
+void	ft_exit(char **args, t_info *info)
+{
+    int	exit_code = 0;
+    int	arg_count = 0;
+
+    while (args[arg_count])
+        arg_count++;
 
     printf("exit\n");
-    if (args[1])
+
+    if (arg_count > 2)
     {
-        if (!ft_isdigit(args[1][0]))
+        write(2, "minishell: exit: too many arguments\n", 36);
+        info->exit_status = 1;
+        return ;
+    }
+    if (arg_count == 2)
+    {
+        if (!is_numeric(args[1]))
         {
             write(2, "exit: ", 6);
-			write(2, args[1], ft_strlen(args[1]));
-			write(2, ": numeric argument required\n", 28);
+            write(2, args[1], ft_strlen(args[1]));
+            write(2, ": numeric argument required\n", 28);
             exit_code = 255;
         }
         else
