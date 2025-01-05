c15283798305d8e2342c3735f6a5279f16aae84e
Vladyslav Mamotenko <vmamoten@1-B-30.42heilbronn.de>
2025-01-05 16:27:41 +0100

    refactoring cd

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,13 +6,12 @@
 /*   By: vmamoten <vmamoten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 14:25:04 by vmamoten          #+#    #+#             */
-/*   Updated: 2025/01/05 15:11:20 by vmamoten         ###   ########.fr       */
+/*   Updated: 2025/01/05 16:26:55 by vmamoten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-// Получение текущего каталога
 int	get_current_directory(char *cwd)
 {
 	if (!getcwd(cwd, PATH_MAX))
@@ -23,7 +22,6 @@ int	get_current_directory(char *cwd)
 	return (1);
 }
 
-// Получение HOME директории
 char	*get_home_directory(t_info *info)
 {
 	char	*home;
@@ -37,52 +35,63 @@ char	*get_home_directory(t_info *info)
 	return (home);
 }
 
-// Получение директории для перехода
-char	*resolve_target_directory(char **args, t_info *info)
+char	*get_directory_from_tilde(char *arg, t_info *info)
 {
 	char	*home;
 	char	*dir;
 
-	if (!args[1] || ft_strcmp(args[1], "~") == 0)
-		return (get_home_directory(info));
-	else if (args[1][0] == '~')
+	home = get_home_directory(info);
+	if (!home)
+		return (NULL);
+	dir = ft_strjoin(home, arg + 1);
+	if (!dir)
+		ft_putendl_fd("minishell: cd: memory allocation failed", 2);
+	return (dir);
+}
+
+char	*get_directory_from_dash(char *arg, t_info *info)
+{
+	char	*dir;
+
+	if (ft_strcmp(arg, "-") == 0)
 	{
-		home = get_home_directory(info);
-		if (!home)
+		dir = get_env_value(info, "OLDPWD");
+		if (!dir || dir[0] == '\0')
+		{
+			ft_putendl_fd("minishell: cd: OLDPWD not set", STDERR_FILENO);
 			return (NULL);
-		dir = ft_strjoin(home, args[1] + 1);
-		if (!dir)
-			ft_putendl_fd("minishell: cd: memory allocation failed", 2);
+		}
+		ft_putendl_fd(dir, STDOUT_FILENO);
 		return (dir);
 	}
-	else if (ft_strcmp(args[1], "-") == 0 || ft_strcmp(args[1], "--") == 0)
-	{
-		if (ft_strcmp(args[1], "-") == 0)
-		{
-			dir = get_env_value(info, "OLDPWD");
-			if (!dir || dir[0] == '\0')
-			{
-				ft_putendl_fd("minishell: cd: OLDPWD not set", STDERR_FILENO);
-				return (NULL);
-			}
-			ft_putendl_fd(dir, STDOUT_FILENO);
-			return (dir);
-		}
+	return (get_home_directory(info));
+}
+
+char	*resolve_target_directory(char **args, t_info *info)
+{
+	if (!args[1] || ft_strcmp(args[1], "~") == 0)
 		return (get_home_directory(info));
-	}
+	if (args[1][0] == '~')
+		return (get_directory_from_tilde(args[1], info));
+	if (ft_strcmp(args[1], "-") == 0 || ft_strcmp(args[1], "--") == 0)
+		return (get_directory_from_dash(args[1], info));
 	return (args[1]);
 }
 
-// Обновление переменных окружения
-int	update_env_vars(t_info *info, char *old_cwd)
+int	update_oldpwd(t_info *info, char *old_cwd)
 {
-	char	cwd[PATH_MAX];
-
 	if (set_env(info, "OLDPWD", old_cwd) == -1)
 	{
 		ft_putendl_fd("minishell: cd: failed to set OLDPWD", 2);
 		return (0);
 	}
+	return (1);
+}
+
+int	update_pwd(t_info *info)
+{
+	char	cwd[PATH_MAX];
+
 	if (!getcwd(cwd, PATH_MAX))
 	{
 		perror("minishell: getcwd");
@@ -96,7 +105,15 @@ int	update_env_vars(t_info *info, char *old_cwd)
 	return (1);
 }
 
-// Переход в директорию
+int	update_env_vars(t_info *info, char *old_cwd)
+{
+	if (!update_oldpwd(info, old_cwd))
+		return (0);
+	if (!update_pwd(info))
+		return (0);
+	return (1);
+}
+
 int	try_change_directory(char *dir)
 {
 	if (chdir(dir) != 0)
@@ -107,39 +124,53 @@ int	try_change_directory(char *dir)
 	return (1);
 }
 
-// Основная функция cd
+int	handle_directory_change(char *dir, char **args, t_info *info)
+{
+	if (!try_change_directory(dir))
+	{
+		info->exit_status = 1;
+		if (args[1] && args[1][0] == '~')
+			free(dir);
+		return (0);
+	}
+	if (args[1] && args[1][0] == '~')
+		free(dir);
+	return (1);
+}
+
+int	validate_cd_input(char **args, char *cwd, t_info *info)
+{
+	if (!args)
+	{
+		ft_putendl_fd("minishell: cd: Invalid arguments", 2);
+		info->exit_status = 1;
+		return (0);
+	}
+	if (!get_current_directory(cwd))
+	{
+		info->exit_status = 1;
+		return (0);
+	}
+	return (1);
+}
+
 void	ft_cd(char **args, t_info *info)
 {
 	char	*dir;
 	char	cwd[PATH_MAX];
 
-	if (!args)
-	{
-		ft_putendl_fd("minishell: cd: Invalid arguments", 2);
-		info->exit_status = 1;
+	if (!validate_cd_input(args, cwd, info))
 		return ;
-	}
-	if (!get_current_directory(cwd))
-	{
-		info->exit_status = 1;
-		return ;
-	}
 	dir = resolve_target_directory(args, info);
 	if (!dir)
 	{
 		info->exit_status = 1;
 		return ;
 	}
-	if (!try_change_directory(dir))
-	{
-		info->exit_status = 1;
-		if (args[1] && args[1][0] == '~')
-			free(dir);
+	if (!handle_directory_change(dir, args, info))
 		return ;
-	}
 	if (!update_env_vars(info, cwd))
 		info->exit_status = 1;
-	if (args[1] && args[1][0] == '~')
-		free(dir);
-	info->exit_status = 0;
+	else
+		info->exit_status = 0;
 }
--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -6,80 +6,64 @@
 /*   By: vmamoten <vmamoten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 14:25:35 by vmamoten          #+#    #+#             */
-/*   Updated: 2025/01/04 15:54:29 by vmamoten         ###   ########.fr       */
+/*   Updated: 2025/01/05 16:05:45 by vmamoten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-// Функция для обработки ошибок вывода команды unset
-static void print_unset_error(char *arg)
+static void	print_unset_error(char *arg)
 {
-    if (arg[0] == '-')
-    {
-        ft_putstr_fd("minishell: unset: -", STDERR_FILENO);
-        ft_putchar_fd(arg[1], STDERR_FILENO); // Первый символ флага после '-'
-        ft_putstr_fd(": invalid option\n", STDERR_FILENO);
-        ft_putendl_fd("unset: usage: unset [-f] [-v] [name ...]", STDERR_FILENO);
-    }
-    else
-    {
-        ft_putstr_fd("minishell: unset: `", STDERR_FILENO);
-        ft_putstr_fd(arg, STDERR_FILENO);
-        ft_putstr_fd("': not a valid identifier\n", STDERR_FILENO);
-    }
+	if (arg[0] == '-')
+	{
+		ft_putstr_fd("minishell: unset: -", STDERR_FILENO);
+		ft_putchar_fd(arg[1], STDERR_FILENO);
+		ft_putstr_fd(": invalid option\n", STDERR_FILENO);
+		ft_putendl_fd("unset: usage: unset [-f] [-v] [name ...]",
+			STDERR_FILENO);
+	}
+	else
+	{
+		ft_putstr_fd("minishell: unset: `", STDERR_FILENO);
+		ft_putstr_fd(arg, STDERR_FILENO);
+		ft_putstr_fd("': not a valid identifier\n", STDERR_FILENO);
+	}
 }
 
-void unset_env(t_exec_command *command, t_info *info)
+void	unset_env(t_exec_command *command, t_info *info)
 {
-    int i;
-    int j;
+	int	i;
+	int	j;
 
-    if (!info || !command || !command->args)
-        return;
-    info->exit_status = 0;
-
-    i = 1;
-    while (command->args[i])
-    {
-        // Проверка на флаг (аргумент, начинающийся с '-')
-        if (command->args[i][0] == '-')
-        {
-            print_unset_error(command->args[i]);
-            info->exit_status = 2; // Как в Bash для неправильного флага
-            return;               // Прерываем выполнение команды
-        }
-
-        // Проверка на валидность имени переменной
-        if (!is_valid_env_key(command->args[i]))
-        {
-            print_unset_error(command->args[i]);
-            info->exit_status = 1; // Как в Bash для недопустимого идентификатора
-            i++;
-            continue;
-        }
-
-        // Удаление переменной из окружения
-        j = 0;
-        while (info->envp[j])
-        {
-            if (env_key_compare(info->envp[j], command->args[i]))
-            {
-                info->envp = remove_env_entry(info->envp, j);
-                break;
-            }
-            j++;
-        }
-        i++;
-    }
+	if (!info || !command || !command->args)
+		return ;
+	info->exit_status = 0;
+	i = 1;
+	while (command->args[i])
+	{
+		if (command->args[i][0] == '-')
+		{
+			print_unset_error(command->args[i]);
+			info->exit_status = 2;
+			return ;
+		}
+		if (!is_valid_env_key(command->args[i]))
+		{
+			print_unset_error(command->args[i]);
+			info->exit_status = 1;
+			i++;
+			continue ;
+		}
+		j = 0;
+		while (info->envp[j])
+		{
+			if (env_key_compare(info->envp[j], command->args[i]))
+			{
+				info->envp = remove_env_entry(info->envp, j);
+				break ;
+			}
+			j++;
+		}
+		i++;
+	}
 }
-
-
-
-
-	// int k = 0;
-	// while (command->args[k])
-	// {
-	// 	printf("args n %d = %s\n", k, command->args[k]);
-	// 	k++;
-	// }
